887b88500ad66a6890ec7830c3ac67f33bcdbd30
JaviXDLOL16 <100722991+JaviXDLOL16@users.noreply.github.com>
2024-12-25 21:44:49 -0600

    add last pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
+import OrdenDeServicio from './pdf/OrdenDeServicio';
 import './styles/global.css';
-//import AppRouter from './routes/types';
+// import AppRouter from './routes/types';
 
-import PrestamoEquipo from './pdf/PrestamoEquipo';
 import { PDFViewer } from '@react-pdf/renderer';
 
 function App() {
 
-  return (
-//<AppRouter/>
-    <PDFViewer  style={{ width: '100%', height: '100vh' }}>
-      <PrestamoEquipo />
-    </PDFViewer>
+return (
+// <AppRouter/>
+<PDFViewer  style={{ width: '100%', height: '100vh' }}>
+  <OrdenDeServicio></OrdenDeServicio>
+</PDFViewer>
   )
 }
 
